6a4c851994da10d8cd69d69df9bfd1b87c2ac733
AreebaAtiq <areeba.atique01@gmail.com>
2025-06-18 10:13:23 +0400

    add exercise 15

---
+++ exercise15.py
@@ -0,0 +1,11 @@
+grades = {
+    "Ali": 88,
+    "Sara": 94,
+    "Zain": 76,
+    "Areeba": 85
+}
+
+total = sum(grades.values())
+average = total / len(grades)
+print(f"Average grade: {average}")
+
