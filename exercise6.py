3eb8c4a01750b318af472488d713dd05ea4d8427
AreebaAtiq <areeba.atique01@gmail.com>
2025-06-18 09:55:23 +0400

    add exercise 6

---
+++ exercise6.py
@@ -0,0 +1,10 @@
+foods = ["Pizza", "Burger", "Sushi", "Pasta", "Ice Cream"]
+
+print("Full list:", foods)
+print("First item:", foods[0])
+print("Last item:", foods[-1])
+
+# Add a new food
+foods.append("Tacos")
+print("Updated list:", foods)
+
